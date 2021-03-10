8d4550f40dd0e943dea98b670391752a1b93c99c
Alexey <ua3mqj@gmail.com>
2021-03-10 12:14:52 +0300

    quick_centipede

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker build -t elixir .
 
-docker tag elixir stor.highloadcup.ru/rally/real_frog
+docker tag elixir stor.highloadcup.ru/rally/quick_centipede
 
-docker push stor.highloadcup.ru/rally/real_frog
+docker push stor.highloadcup.ru/rally/quick_centipede
